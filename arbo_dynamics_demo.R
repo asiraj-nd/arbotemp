6b8acb477fa45db9f640ce836fcf732dd80b64f3
Amir Siraj <amirsiraj@macbook-pro-2.dhcp.nd.edu>
2016-08-08 14:48:53 -0400

    Upate README.md

--- arbo_dynamics_demo.R
+++ arbo_dynamics_demo.R
@@ -6,6 +6,7 @@ setwd(dirname(list.files(pattern='epidemicGrowth.RData', recursive=TRUE, full.na
 load("./epidemicGrowth.Rdata")
 
 # Syntax
+set.seed(123)
 gt.function(25)(0:50) ##(temperature in oC) (time_1: time_n)    
 formalR0(25,0.4)   ## (temperature in oC, lambda)
 solve.r(32.9,0.2)  ## (temperature in oC, lambda)
@@ -14,8 +15,8 @@ solve.r(32.9,0.2)  ## (temperature in oC, lambda)
 ####### Demo #####################
 
 # generation interval
-plot(gt.function(20)(1:100), type='l') ## using the default time length (120 days)
-lines(gt.function(20,,402,.01)(1:100),lty=2)  ## custom time length
+plot(gt.function(20)(1:400), type='l') ## using the default time length (120 days)
+lines(gt.function(20,,400,.01)(1:400),lty=2)  ## custom time length (400 days)
 
 # R0
 formalR0(32,1.35)  # custom default virus (DENV)
