46012be52ad3a25fde4676c400e3ad0ee567faf1
Amir Siraj <amirsiraj@macbook-pro-2.dhcp.nd.edu>
2016-08-08 15:00:35 -0400

    Upate README.md

--- arbo_dynamics_demo.R
+++ arbo_dynamics_demo.R
@@ -6,23 +6,26 @@ setwd(dirname(list.files(pattern='epidemicGrowth.RData', recursive=TRUE, full.na
 load("./epidemicGrowth.Rdata")
 
 # Syntax
-set.seed(123)
-gt.function(25)(0:50) ##(temperature in oC) (time_1: time_n)    
-formalR0(25,0.4)   ## (temperature in oC, lambda)
-solve.r(32.9,0.2)  ## (temperature in oC, lambda)
+
+#     gt.function(temperature)(time) where (temperature in oC)  time is a vector (time_0: time_n)    
+
+#     formalR0(temperature,lambda)  where (temperature in oC, lambda is the daily number of new adults mosquitoes per person)
+
+#     solve.r(temperature,lamda)  ## where (temperature in oC, lambda)
 
 
 ####### Demo #####################
+set.seed(123)
 
 # generation interval
-plot(gt.function(20)(1:400), type='l') ## using the default time length (120 days)
-lines(gt.function(20,,400,.01)(1:400),lty=2)  ## custom time length (400 days)
+plot(gt.function(25)(1:400), type='l') ## at 25 oC, using the default time length (120 days)
+lines(gt.function(25,,400,.01)(1:400),lty=2)  ## at 25 oC, custom time length (400 days)
 
 # R0
-formalR0(32,1.35)  # custom default virus (DENV)
-formalR0(32,1.35,2,,)  # custom virus (CHIKV)
+formalR0(32,1.35)  # at 32 oC, default virus (DENV)
+formalR0(32,1.35,2,,)  # at 32 oC, custom virus (2=CHIKV, temporarily set with DENV parameters)
 
 # epidemic growth
 system.time(solve.r(25,0.35)) # at 25 oC and lambda=0.35, using the default r range = [-0.15, 0.15]
-solve.r(32,1.35,,-0.15,.25)  # 32 oC and lambda = 1.35, using custom range = [-0.15, 0.25]
+solve.r(32,1.35,,-0.15,.25)  # at 32 oC and lambda = 1.35, using custom range = [-0.15, 0.25]
 
